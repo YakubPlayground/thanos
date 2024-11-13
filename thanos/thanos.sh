5eb72cf301124c2580030cef712c28d9d67de69f
YakubPlayground <46232900+YakubPlayground@users.noreply.github.com>
2024-11-13 03:54:40 +0000

    refactor: update Thanos configuration and deployment files with new proxy settings and services

--- thanos/thanos.sh
+++ thanos/thanos.sh
@@ -12,18 +12,25 @@ helm repo list | grep -q 'bitnami' || helm repo add bitnami https://charts.bitna
 # Update Helm repositories
 helm repo update
 
-# Install or upgrade the Thanos Helm chart with the external prefix
-helm upgrade --install thanos-release bitnami/thanos --namespace monitoring-testing-october24 --set query.args="{--web.external-prefix=/thanos}"
+# Install or upgrade the Thanos Helm chart with the external prefix and additional components
+helm upgrade --install thanos-release bitnami/thanos --namespace monitoring-testing-october24 \
+  --values /workspaces/thanos/thanos/app/values.yaml
 
 # Create a symbolic link if it doesn't already exist
-[ -L /etc/nginx/conf.d/thanos.conf ] || sudo ln -s /workspaces/thanos/app/thanos.conf /etc/nginx/conf.d/thanos.conf
+[ -L /etc/nginx/conf.d/thanos.conf ] || sudo ln -s /workspaces/thanos/thanos/app/thanos.conf /etc/nginx/conf.d/thanos.conf
+
+# Restart nginx to apply the new configuration
+sudo systemctl restart nginx
 
 # Check the status of the pods
 kubectl get pods -n monitoring-testing-october24
 
 # Forward ports (example: forwarding port 9090 of the Thanos Query pod to localhost:9090)
 kubectl port-forward -n monitoring-testing-october24 svc/thanos-release-query 9090:9090 &
-kubectl port-forward -n monitoring-testing-october24 svc/thanos-release-storegateway 10901:10901 &
+kubectl port-forward -n monitoring-testing-october24 svc/thanos-release-storegateway 10902:10901 &
+kubectl port-forward -n monitoring-testing-october24 svc/thanos-release-receiver 10903:10901 &
+kubectl port-forward -n monitoring-testing-october24 svc/thanos-release-frontend 9091:9091 &
+kubectl port-forward -n monitoring-testing-october24 svc/thanos-release-readwrite 9092:9092 &
 
 # Check the status of the services
 kubectl get service -n monitoring-testing-october24
